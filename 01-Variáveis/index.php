ef2bddd02392957641f06efd7a24be33096c7a9c
Igor <igor.torquato.20@gmail.com>
2024-12-11 18:32:54 -0300

    add: variables

---
+++ 01-Variáveis/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $name = " João "
+    ?>
+    <h1>Meu nome é <?php echo $name ?></h1>
+    <p> <?= $name?> tem 2 filhos</p>
+</body>
+</html>
